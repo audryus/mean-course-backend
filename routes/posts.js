078b2fce6c15ed5aeed01adc9218b0104614c0e3
Aubay Aml <amleite@aubay.com>
2020-03-05 11:56:24 +0000

    Added pagination support

--- routes/posts.js
+++ routes/posts.js
@@ -1,15 +1,14 @@
-const express = require("express")
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-const Post = require("../model/post")
+const Post = require("../model/post");
 const multer = require("multer");
-
 
 const MIME_TYPE_MAP = {
   "image/png": "png",
   "image/jpeg": "jpg",
   "image/jpg": "jpg"
-}
+};
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -21,17 +20,20 @@ const storage = multer.diskStorage({
     cb(error, "./images");
   },
   filename: (req, file, cb) => {
-    const name = file.originalname.toLowerCase().split(" ").join("-");
+    const name = file.originalname
+      .toLowerCase()
+      .split(" ")
+      .join("-");
     const ext = MIME_TYPE_MAP[file.mimetype];
     cb(null, name + "-" + Date.now() + "-" + ext);
   }
 });
 
-var getPostModel = (req) => {
+var getPostModel = req => {
   const post = req.body;
   let postModel = new Post({
-      title: post.title,
-      content: post.content
+    title: post.title,
+    content: post.content
   });
   if (post.id && post.id !== "null") {
     postModel._id = post.id;
@@ -41,63 +43,77 @@ var getPostModel = (req) => {
   }
 
   return postModel;
-}
+};
 
-var getUrl = (req) => {
-  return req.protocol + "://"+ req.get("host");
-}
+var getUrl = req => {
+  return req.protocol + "://" + req.get("host");
+};
 
 var setImagePath = (postModel, req) => {
-  postModel.imagePath = getUrl(req) + "/images/" + req.file.filename
-}
+  postModel.imagePath = getUrl(req) + "/images/" + req.file.filename;
+};
 
-router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
-    const post = req.body
+router.post("", multer({ storage: storage }).single("image"), (req, res, next) => {
+    const post = req.body;
     const postModel = getPostModel(req);
     postModel.save().then(savedData => {
-        post.id = savedData._id
-        res.status(201).json({
-            message: "Sucess",
-            posts: [post]
-        })
-    })
-})
+      post.id = savedData._id;
+      res.status(201).json({
+        message: "Sucess",
+        posts: [post]
+      });
+    });
+  }
+);
 
-router.patch("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
-    const postModel = getPostModel(req)
-    Post.updateOne({_id: req.params.id}, postModel)
-        .then(result => {
-            res.status(200).json({message: 'ok'})
-        })
-})
+router.patch("/:id", multer({ storage: storage }).single("image"), (req, res, next) => {
+    const postModel = getPostModel(req);
+    Post.updateOne({ _id: req.params.id }, postModel).then(result => {
+      res.status(200).json({ message: "ok" });
+    });
+  }
+);
 
 router.get("", (req, res, next) => {
-    Post.find().then(documents => {
-        res.status(200).json({
-            message: 'Post fetched sucess', 
-            posts: documents
-        })
-    })
+  const query = Post.find();
+  const pageSize = +req.query.pagesize;
+  const currentPage = +req.query.page;
+
+  if (pageSize && currentPage) {
+    query
+      .skip(pageSize * (currentPage - 1))
+      .limit(pageSize);
+  }
+  let fetchedDocuments;
+  query.then(documents => {
+    fetchedDocuments = documents;
+    return Post.countDocuments();
+  }).then(count => {
+    res.status(200).json({
+      message: "Post fetched sucess",
+      posts: fetchedDocuments,
+      count: count
+    });
+  });
 });
 
 router.get("/:id", (req, res, next) => {
-    Post.findById(req.params.id).then(document => {
-        if (document) {
-            res.status(200).json({
-                message: 'Post fetched sucess', 
-                posts: [document]
-            })
-        } else {
-            res.status(404).json({ message: "Post not found."})
-        }
-    })
+  Post.findById(req.params.id).then(document => {
+    if (document) {
+      res.status(200).json({
+        message: "Post fetched sucess",
+        posts: [document]
+      });
+    } else {
+      res.status(404).json({ message: "Post not found." });
+    }
+  });
 });
 
 router.delete("/:id", (req, res, next) => {
-    Post.deleteOne({_id: req.params.id})
-        .then(result => {
-            res.status(200).json(result)
-        })
-})
+  Post.deleteOne({ _id: req.params.id }).then(result => {
+    res.status(200).json(result);
+  });
+});
 
 module.exports = router;
