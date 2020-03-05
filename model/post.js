cca29876e4616d8017856631287b54a378567630
Aubay Aml <amleite@aubay.com>
2020-03-05 10:16:53 +0000

    Added images access. Image uploading support and fixing.

--- model/post.js
+++ model/post.js
@@ -8,6 +8,10 @@ const schema = mongoose.Schema({
     content: {
         type: String,
         required: true 
+    },
+    imagePath: {
+        type: String,
+        required: true 
     }
 })
 
--- routes/posts.js
+++ routes/posts.js
@@ -23,25 +23,37 @@ const storage = multer.diskStorage({
   filename: (req, file, cb) => {
     const name = file.originalname.toLowerCase().split(" ").join("-");
     const ext = MIME_TYPE_MAP[file.mimetype];
-    console.log("saving image")
     cb(null, name + "-" + Date.now() + "-" + ext);
   }
 });
 
-var getPostModel = (post) => {
-    return new Post({
-        _id: post.id,
-        title: post.title,
-        content: post.content
-    })
+var getPostModel = (req) => {
+  const post = req.body;
+  let postModel = new Post({
+      title: post.title,
+      content: post.content
+  });
+  if (post.id && post.id !== "null") {
+    postModel._id = post.id;
+  }
+  if (req.file) {
+    setImagePath(postModel, req);
+  }
+
+  return postModel;
+}
+
+var getUrl = (req) => {
+  return req.protocol + "://"+ req.get("host");
+}
+
+var setImagePath = (postModel, req) => {
+  postModel.imagePath = getUrl(req) + "/images/" + req.file.filename
 }
 
 router.post("", multer({storage: storage}).single("image"), (req, res, next) => {
     const post = req.body
-    const postModel = new Post({
-        title: post.title,
-        content: post.content
-    })
+    const postModel = getPostModel(req);
     postModel.save().then(savedData => {
         post.id = savedData._id
         res.status(201).json({
@@ -51,8 +63,8 @@ router.post("", multer({storage: storage}).single("image"), (req, res, next) => 
     })
 })
 
-router.patch("/:id", (req, res, next) => {
-    const postModel = getPostModel(req.body)
+router.patch("/:id", multer({storage: storage}).single("image"), (req, res, next) => {
+    const postModel = getPostModel(req)
     Post.updateOne({_id: req.params.id}, postModel)
         .then(result => {
             res.status(200).json({message: 'ok'})
